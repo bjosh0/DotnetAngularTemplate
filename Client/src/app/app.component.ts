c2cded69e2af28a633dd12949536ec8dc1300f5c
bjosh0 <bjosh0@aol.com>
2024-01-23 20:37:36 +0000

    Refactor app component to leverage weather service

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from "../services/weather.service";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  protected weatherData: any[] = [];
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData().subscribe((data) => {
+      this.weatherData = data;
+    })
+  }
+
 }
